b5a070bdf3c9b81a2e3ff190d83c6b3d5039b615
krishnaa-14 <68679770+krishnaa-14@users.noreply.github.com>
2023-04-16 00:56:36 +0530

    Intial commit For search functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
+import React from "react";
+import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [myList, setMyList] = useState(["Apple", "Samsung", "Motorola", "Oppo", "Real me", "IQ", "Pineapple"]);
+  const [filteredList, setFilteredList] = useState(["Apple", "Samsung", "Motorola", "Oppo", "Real me", "IQ", "Pineapple"]);
+  const [searchText, setSearchText] = useState("");
+
+  function getFilteredItems(searchText) {
+
+    if(searchText === "") {
+      return myList;
+    }
+
+    return myList.filter((item) => 
+      item.toLowerCase().includes(searchText.toLowerCase())
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+    <h1 className="text-center my-3">Search Feature React</h1>
+    <div className="input-group mb-3">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search Here"
+        value={searchText}
+        onChange={(e) => {
+          setSearchText(e.target.value);
+          setFilteredList(getFilteredItems(e.target.value));
+        }}
+      />
+      {/*
+      <div className="input-group-append">
+        <button
+          className="btn btn-dark"
+          type="button"
+          onClick={() => setFilteredList(getFilteredItems(searchText))}
         >
-          Learn React
-        </a>
-      </header>
+          Search
+        </button>
+      </div>
+      */}
     </div>
+    {filteredList.map((item) => {
+      return (
+        <div key={item}>
+          <ul className="list-group">
+            <li className="list-group-item">{item}</li>
+          </ul>
+        </div>
+      );
+    })}
+  </div>
   );
 }
 
